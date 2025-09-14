fe13ad1f04e31ab45771499d3b093058116396b5
kitimark <markdog_@hotmail.com>
2025-09-14 19:18:36 +0700

    refactor hw2 q2

--- hw2_kiti_mark_2.rkt
+++ hw2_kiti_mark_2.rkt
@@ -3,7 +3,6 @@
 
 (require 2htdp/image)
 
-; # question 2, draw rose shape
 (define s 500)
 
 (define (half x)
@@ -12,31 +11,36 @@
 (define (find-hypotenuse-side opposite adjacent)
     (sqrt (+ (sqr opposite) (sqr adjacent))))
 
-(define (find-hypotenuse-of-equal-side s)
+(define (find-hypotenuse-with-equal-side s)
     (find-hypotenuse-side s s))
 
 (define (find-side-of-inner-square s)
-    (find-hypotenuse-of-equal-side (half s)))
+    (find-hypotenuse-with-equal-side (half s)))
 
-(define (draw-rose-sepals s)
-    (square s "solid" "seagreen"))
+(define (draw-rose-sepals frame-size)
+    (overlay
+        (square frame-size "outline" "white")
+        (square frame-size "solid" "seagreen")))
 
-(define (draw-rose-petals s)
+(define (draw-rose-petals frame-size)
     (overlay
         (rotate 45
-            (draw-rose-sepals (find-hypotenuse-of-equal-side (half s))))
-        (circle (half s) "solid" "light red")))
+            (draw-rose-sepals (find-hypotenuse-with-equal-side (half frame-size))))
+        (overlay
+            (circle (half frame-size) "outline" "white")
+            (circle (half frame-size) "solid" "light red"))))
 
-(define (draw-rose-flower-with-recursion-with-frame-size s n)
-    (define (draw-rose-petals-nest s n)
-        (if (= n 1)
-            (draw-rose-petals s)
+; # question 2, draw rose shape
+(define (draw-rose-flower-with-recursion-with-frame-size frame-size number-of-petals)
+    (define (draw-rose-petals-nest frame-size number-of-petals)
+        (if (= number-of-petals 1)
+            (draw-rose-petals frame-size)
             (underlay
-                (draw-rose-petals s)
+                (draw-rose-petals frame-size)
                 (rotate 45
-                    (draw-rose-petals-nest (find-side-of-inner-square s) (- n 1))))))
+                    (draw-rose-petals-nest (find-side-of-inner-square frame-size) (- number-of-petals 1))))))
     (overlay
-        (draw-rose-petals-nest s n)
-        (draw-rose-sepals s)))
+        (draw-rose-petals-nest frame-size number-of-petals)
+        (draw-rose-sepals frame-size)))
 
 (draw-rose-flower-with-recursion-with-frame-size s 3)
